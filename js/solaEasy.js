cecb52b7435ebd9eee3c8a64adfb83a5b9e6d79d
alantrue <alantruekimo@yahoo.com.tw>
2017-11-14 14:20:08 +0800

    solaEasy wallet page

--- js/solaEasy.js
+++ js/solaEasy.js
@@ -1,5 +1,31 @@
 $(function() {
+
+	bindAll();
+	
     for (var i = 0; i < 20; ++i) {
         $(".historyTable").append("<tr><td>BID</td><td>Completed</td><td>2017/11/11 11:11:11</td><td>SNTW</td><td>123.99</td><td>12,399</td></tr>");
     }
+
+    $("#menuDashboard").trigger("click");
 });
+
+function bindAll() {	
+    $("#menuDashboard").click(function() {
+    	$(".mainBody").children().hide();
+    	$("#dashboard").show();
+    });
+
+    $("#menuWallet").click(function() {
+    	$(".mainBody").children().hide();
+    	$("#wallet").show();
+    });
+
+    $("#menuTrade").click(function() {
+    	$(".mainBody").children().hide();
+    	$("#trade").show();
+    });
+
+    $("#menuLogout").click(function() {
+
+    });
+}
