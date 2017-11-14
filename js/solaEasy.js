18d7da0e05fdcbf9e401ca06ed8ca96b85f83e72
alantrue <alantruekimo@yahoo.com.tw>
2017-11-14 13:01:31 +0800

    solaEasy page1

---
+++ js/solaEasy.js
@@ -0,0 +1,5 @@
+$(function() {
+    for (var i = 0; i < 20; ++i) {
+        $(".historyTable").append("<tr><td>BID</td><td>Completed</td><td>2017/11/11 11:11:11</td><td>SNTW</td><td>123.99</td><td>12,399</td></tr>");
+    }
+});
